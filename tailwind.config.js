4cccc119cabaaf3e9221c3a7a510a24e616fae27
Natanael Melo <natanael.melo47@gmail.com>
2025-09-24 20:26:47 -0300

    remove

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,12 +8,44 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: '#2C7B5D', // verde profundo
-        secondary: '#6168A6', // azul arroxeado
-        accent: '#F2A03D', // laranja quente
-        background: '#F5F5F5', // cinza claro
-        foreground: '#333333', // texto escuro
-        white: '#FFFFFF',
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
       },
     },
   },
