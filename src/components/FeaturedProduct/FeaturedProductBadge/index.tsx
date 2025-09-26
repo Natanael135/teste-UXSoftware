7bf1655fe038197508c3a53a8491f541473abd0a
Natanael Melo <natanaelmelo.dev@gmail.com>
2025-09-26 11:32:45 -0300

    remove

--- src/components/FeaturedProduct/FeaturedProductBadge/index.tsx
+++
@@ -1,5 +0,0 @@
-export function FeaturedProductBadge() {
-  return (
-    <span className="uppercase text-xs font-semibold text-secondary tracking-widest">Produto em destaque</span>
-  );
-}
--- src/components/FeaturedProduct/FeaturedProductDescription/index.tsx
+++
@@ -1,9 +0,0 @@
-interface FeaturedProductDescriptionProps {
-  description: string;
-}
-
-export function FeaturedProductDescription({ description }: FeaturedProductDescriptionProps) {
-  return (
-    <p className="text-muted-foreground text-lg max-w-xl">{description}</p>
-  );
-}
